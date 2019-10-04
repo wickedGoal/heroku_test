561e4275d55f507fedc7696b9a00bb06f26a557c
Hyunjae Sung <jae_k@hotmail.com>
2019-10-04 18:50:05 +0900

    cors added update

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 
 const express = require("express");
 var app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const path = require("path");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
@@ -26,11 +27,12 @@ db.once("open", function callback() {
 
 //.use(express.static(path.join(__dirname, 'public')))
 
+app.use(cors());
 //Body-parser substitute: Express's parser (raw, txt parse needs body-parser))
 //app.use(express.json());
 //app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //React client
 app.use(express.static(path.join(__dirname, "client/build")));
