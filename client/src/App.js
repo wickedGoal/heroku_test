5df56d29a7a80223f2333b2a60939a2b14e3fdc0
Hyunjae Sung <jae_k@hotmail.com>
2019-10-04 18:48:50 +0900

    cors added

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a
-              class="navbar-brand"
+              className="navbar-brand"
               href="https://codingthesmartway.com"
               target="_blank"
             >
